458e40340339d886cba4838c41e73a541a3aa012
Welcome <welcomegalane@gmail.com>
2026-02-10 14:50:18 +0200

    Created basic local storage service

---
+++ js/services/local-storage.js
@@ -0,0 +1,24 @@
+class LocalStorageService{
+    constructor(){
+
+    }
+
+    static createUser(username,password){
+        let users=JSON.parse(localStorage.getItem('users'))||[];
+        const user=new User(username,password);
+        users.push(user);
+        localStorage.setItem(JSON.stringify(users));
+    }
+
+    static createChat(userId1,userId2){
+        let chats=JSON.parse(localStorage.getItem('chats'));
+        let chat=new Chat(userId1,userId2);
+        chats.push(chat);
+        localStorage.setItem(JSON.stringify(chats));        
+    }
+
+    static createGroup(){
+        
+    }
+
+}
